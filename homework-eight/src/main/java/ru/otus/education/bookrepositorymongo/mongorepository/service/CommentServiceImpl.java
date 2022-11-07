89b80c3f5ab34a833234a8324154afeeecba727a
Dmitry Gonyak dgonyak@iitdgroup.ru <hunkizzzx777@gmail.com>
2022-11-07 21:05:31 +0300

    homework-eight

--- homework-eight/src/main/java/ru/otus/education/bookrepositorymongo/mongorepository/service/CommentServiceImpl.java
+++ homework-eight/src/main/java/ru/otus/education/bookrepositorymongo/mongorepository/service/CommentServiceImpl.java
@@ -2,18 +2,15 @@ package ru.otus.education.bookrepositorymongo.mongorepository.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.otus.education.bookrepositorymongo.mongorepository.repo.BookRepo;
 import ru.otus.education.bookrepositorymongo.mongorepository.repo.CommentRepo;
 
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final CommentRepo commentRepo;
-    private final BookRepo bookRepo;
 
     @Override
     public void deleteById(long id) {
         commentRepo.deleteById(id);
-//        bookRepo.deleteCommentById(id);
     }
 }
